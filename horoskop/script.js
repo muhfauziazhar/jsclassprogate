92c1c73bd72edf735628720965f121e94ae66cc4
muhfauziazhar <muhammad.fauzi73@ui.ac.id>
2022-02-02 11:55:20 +0700

    first commit

---
+++ horoskop/script.js
@@ -0,0 +1,27 @@
+// const randomNumber = Date.now();
+// console.log(randomNumber);
+
+const btn = document.querySelector("button");
+btn.addEventListener("click", function () {
+  this.innerHTML = "Coba lagi";
+  this.style.color = "red";
+
+  let number = Date.now();
+  console.log(number);
+  function horoscope(randomNumber) {
+    if (randomNumber % 5 == 0) {
+      return "Hebat!!";
+    } else if (randomNumber % 2 == 0) {
+      return "Boleh juga!";
+    } else if (randomNumber % 3 == 0) {
+      return "Mengerikan";
+    } else {
+      return "Coba lagi!";
+    }
+  }
+
+  const hasil = document.getElementById("hasil");
+  const newPara = document.createElement("p");
+  newPara.innerHTML = horoscope(number);
+  hasil.append(newPara);
+});
